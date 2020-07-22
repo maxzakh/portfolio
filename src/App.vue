046af46f20c8e00f5864e7fc09bcdebe3f7f1070
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-07-22 16:29:46 -0700

    Added footer with sosh med

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,20 @@
             <router-link to="/about">About</router-link>
         </div>
         <router-view />
+        <footer class="page-footer">
+            <p>Find me on</p>
+            <div class="social-media">
+                <a href="https://github.com/maxzakh" target="_blank">
+                    <img src="./assets/github_icon.svg" alt="GitHub" />
+                </a>
+                <a href="https://twitter.com/maxamoose9">
+                    <img src="./assets/twitter_icon.svg" alt="Twitter" />
+                </a>
+                <a href="https://www.linkedin.com/in/max-zakharzhevskiy-08097050/" target="_blank">
+                    <img src="./assets/linkedin_icon.svg" alt="LinkedIn" />
+                </a>
+            </div>
+        </footer>
     </div>
 </template>
 
@@ -21,7 +35,7 @@ html {
 
 body {
     font-family: "Montserrat", sans-serif;
-    background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNSIgaGVpZ2h0PSIxNSI+CjxyZWN0IHdpZHRoPSI1MCIgaGVpZ2h0PSI1MCIgZmlsbD0iIzI4MjgyOCI+PC9yZWN0Pgo8Y2lyY2xlIGN4PSIzIiBjeT0iNC4zIiByPSIxLjgiIGZpbGw9IiMzOTM5MzkiPjwvY2lyY2xlPgo8Y2lyY2xlIGN4PSIzIiBjeT0iMyIgcj0iMS44IiBmaWxsPSJibGFjayI+PC9jaXJjbGU+CjxjaXJjbGUgY3g9IjEwLjUiIGN5PSIxMi41IiByPSIxLjgiIGZpbGw9IiMzOTM5MzkiPjwvY2lyY2xlPgo8Y2lyY2xlIGN4PSIxMC41IiBjeT0iMTEuMyIgcj0iMS44IiBmaWxsPSJibGFjayI+PC9jaXJjbGU+Cjwvc3ZnPg==");
+    // background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNSIgaGVpZ2h0PSIxNSI+CjxyZWN0IHdpZHRoPSI1MCIgaGVpZ2h0PSI1MCIgZmlsbD0iIzI4MjgyOCI+PC9yZWN0Pgo8Y2lyY2xlIGN4PSIzIiBjeT0iNC4zIiByPSIxLjgiIGZpbGw9IiMzOTM5MzkiPjwvY2lyY2xlPgo8Y2lyY2xlIGN4PSIzIiBjeT0iMyIgcj0iMS44IiBmaWxsPSJibGFjayI+PC9jaXJjbGU+CjxjaXJjbGUgY3g9IjEwLjUiIGN5PSIxMi41IiByPSIxLjgiIGZpbGw9IiMzOTM5MzkiPjwvY2lyY2xlPgo8Y2lyY2xlIGN4PSIxMC41IiBjeT0iMTEuMyIgcj0iMS44IiBmaWxsPSJibGFjayI+PC9jaXJjbGU+Cjwvc3ZnPg==");
 }
 
 #app {
@@ -42,6 +56,23 @@ body {
         padding: 0.5em;
         color: white;
         text-decoration: none;
+    }
+}
+
+.page-footer {
+    background-color: rgb(141, 104, 175);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1%;
+}
+
+.social-media {
+    width: 10%;
+    padding: 1%;
+
+    a {
+        padding-right: 5%;
     }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="home">
-        <div>
-          Home
-        </div>
+        <img id="tracks" width="120px" height="120px" src="../assets/moose-tracks.svg">
+
         <svg class="link-a link-shape" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M12,22.57L8.89,23.52L5.98,22.32L3.49,20.41L1.58,17.92L0.38,15.01L1.33,11.9L0.38,8.79L1.58,5.88L3.49,3.39L5.98,1.48L8.89,0.28L12,1.23L15.11,0.28L18.02,1.48L20.51,3.39L22.42,5.88L23.62,8.79L22.67,11.9L23.62,15.01L22.42,17.92L20.51,20.41L18.02,22.32L15.11,23.52z"
@@ -66,6 +65,13 @@ path {
     position: absolute;
     top: 0px;
     left: 100px;
+}
+.page-header {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    width: 100%;
+    padding: 20px;
 }
 
 @keyframes moving {
