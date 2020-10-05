138f3db2d1cafb234d1e828db105792a3a734520
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-10-05 16:25:45 -0700

    reduced png size, about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,12 @@
     </h1>
     <p>I am the web developer for Kcrafts, an eCommerce website that sells genuine gemstone jewelry</p>
     <br>
+    <p>I got into programming to make my life easier.</p>
+    <br>
+    <p>I started by automating formatting for Microsoft Word and Excel by writing functions using VBA</p>
+    <br>
+    <p>Stop wasting time formating documents, when you can be doing other tasks</p>
+    <br>
     <img src="../../src/assets/kcrafts-preview.png" alt="picture of shopping page">
   </div>
 </template>
