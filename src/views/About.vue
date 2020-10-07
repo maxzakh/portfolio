3aa40329a600689f87942e01246b379d77a1e562
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-10-07 00:42:27 -0700

    About me copy

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,13 +3,12 @@
     <h1>
       About
     </h1>
-    <p>I am the web developer for Kcrafts, an eCommerce website that sells genuine gemstone jewelry</p>
+    <p>I am the web developer for Kcrafts, an eCommerce website that sells genuine gemstone jewelry.</p>
     <br>
     <p>I got into programming to make my life easier.</p>
+    <p>I started by automating formatting for Microsoft Word and Excel by writing functions using VBA.</p>
     <br>
-    <p>I started by automating formatting for Microsoft Word and Excel by writing functions using VBA</p>
-    <br>
-    <p>Stop wasting time formating documents, when you can be doing other tasks</p>
+    <p>Now I create and maintain websites, develop new features </p>
     <br>
     <img src="../../src/assets/kcrafts-preview.png" alt="picture of shopping page">
   </div>
